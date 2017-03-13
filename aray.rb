46a563eea13ecc414c2277f2fb4f6acd7dbe9673
ight <thelight.prakash247@gmail.com>
2017-03-14 05:22:18 +0530

    Practice: Array creation and access by index

---
+++ aray.rb
@@ -0,0 +1,14 @@
+#Array
+
+def print_array
+  # Intialize an empty array
+  a = []
+  # Intialize array with elements
+  a = [1,2.2, "name",nil]
+  puts a[0] # Index zero element
+  puts a[1] # Index 1st element
+  puts a[2] # Index 2nd element
+  puts a[3] # Index 3rd element
+end
+
+print_array
