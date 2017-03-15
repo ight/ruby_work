fdad09e61e3bd797644f9d715b6cf4074358492b
prakash bhakat <prakash.bhakat2@tcs.com>
2017-03-15 14:22:35 +0530

    Practice: Some hash and condition

---
+++ hash.rb
@@ -0,0 +1,10 @@
+# Hash creation and access
+just_play = {
+  'foot_ball' => 'outdoor',
+  'cricket' => 'outdoor',
+  'hockey' => 'outdoor',
+  'video_game' => 'indoor'
+}
+
+puts just_play['foot_ball']
+puts just_play['video_game']
